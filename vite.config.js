04336f4c0021f230f2313ff24820097197ab7709
muhammedthameem <muhammedthameem.m@gmail.com>
2026-05-13 12:58:54 +0530

    convert like app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Classy ERP',
+        short_name: 'ClassyERP',
+        description: 'Boutique ERP App',
+        theme_color: '#000000',
+
+        icons: [
+          {
+            src: '/logo-black.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo-black.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
